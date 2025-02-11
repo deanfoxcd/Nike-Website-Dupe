0596fb8fbe745a702657711b70cff234185e62c7
Dean Fox <deanfoxcd@gmail.com>
2025-02-10 19:48:30 -0500

    add popular products cards

--- src/App.jsx
+++ src/App.jsx
@@ -12,30 +12,30 @@ import {
 
 export default function App() {
   return (
-    <main className='relative'>
+    <main className="relative">
       <Nav />
-      <section className='xl:padding-1 wide:padding-r padding-b'>
+      <section className="xl:padding-l wide:padding-r padding-b">
         <Hero />
       </section>
-      <section className='padding'>
+      <section className="padding">
         <PopularProducts />
       </section>
-      <section className='padding'>
+      <section className="padding">
         <SuperQuality />
       </section>
-      <section className='padding-x py-10'>
+      <section className="padding-x py-10">
         <Services />
       </section>
-      <section className='padding'>
+      <section className="padding">
         <SpecialOffers />
       </section>
-      <section className='bg-pale-blue padding'>
+      <section className="padding bg-pale-blue">
         <CustomerReviews />
       </section>
-      <section className='padding-x sm:py-32 py-16 w-full'>
+      <section className="padding-x w-full py-16 sm:py-32">
         <Subscribe />
       </section>
-      <section className='bg-black padding-x padding-t pb-8 text-white'>
+      <section className="padding-x padding-t bg-black pb-8 text-white">
         <Footer />
       </section>
     </main>

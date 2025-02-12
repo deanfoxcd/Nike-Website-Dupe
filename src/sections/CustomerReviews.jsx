4369087916c6b07fd8e4886a989b61c22954ee3a
Dean Fox <deanfoxcd@gmail.com>
2025-02-11 21:30:40 -0500

    add customer reviews

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,5 +1,33 @@
+import ReviewCard from '../components/ReviewCard';
+import { reviews } from '../constants';
+
 function CustomerReviews() {
-  return <div>Customer Reviews</div>;
+  return (
+    <section className="max-container">
+      <h3 className="text-center font-palanquin text-4xl font-bold">
+        What do our <span className="text-coral-red">Customers</span> have to
+        say?
+      </h3>
+
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Hear genuine stories from our satisfied customers
+      </p>
+
+      <div className="mt-24 flex flex-1 items-center justify-evenly gap-14 max-lg:flex-col">
+        {reviews.map((review) => {
+          return (
+            <ReviewCard
+              key={review.customerName}
+              imgURL={review.imgURL}
+              customerName={review.customerName}
+              rating={review.rating}
+              feedback={review.feedback}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
 export default CustomerReviews;
